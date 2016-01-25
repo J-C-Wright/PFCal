4dcd537be0df95c4cc799c181a7f926d84456d00
J-C-Wright <jack.charles.wright@cern.ch>
2016-01-25 15:58:54 +0100

    Commit before trying out squares again

--- PFCalEE/analysis/src/HexagonalGeometry.cc
+++ PFCalEE/analysis/src/HexagonalGeometry.cc
@@ -9,7 +9,6 @@
 
 #include "HGCSSGenParticle.hh"
 #include "HGCSSRecoHit.hh"
-#include "TrackTruthProducer.hh"
 
 #include "HexagonalGeometry.hh"
 
@@ -32,7 +31,7 @@
 
     std::pair<unsigned,unsigned> Hexagon::getNearestEdges_XY(ROOT::Math::XYPoint hit) {
 
-        std::pair<unsigned,unsigned> closestEdges(999,999);
+        std::pair<unsigned,unsigned> closestEdges(9999,9999);
         //First is closest in x, second is closest in y
         //See personal notes, 7/1/16 pg 2
 
@@ -71,62 +70,65 @@
         }
 
         return closestEdges;
-
     }
 
     std::pair<float,float> Hexagon::getDistanceToEdges_XY(ROOT::Math::XYPoint hit) {
 
         std::pair<unsigned,unsigned> closestEdges = getNearestEdges_XY(hit); 
         std::pair<float,float> centreDisplacement = getDisplacementFromCentre_XY(hit);
-        std::pair<float,float> distanceToEdges(-999.,-999.);
+        std::pair<float,float> distanceToEdges(9999.,9999.);
 
-        //Is it in the central region of the hex?
-        if (closestEdges.first != closestEdges.second) {
-
-            //Y
-            distanceToEdges.second = centreDisplacement.second - edgeSize_*sqrt(3);
-            if ( centreDisplacement.second < 0 ) distanceToEdges.second *= -1.0;
-            //X
-            distanceToEdges.first = centreDisplacement.first - (edgeSize_/2.0) - fabs(distanceToEdges.second)/sqrt(3);
-            if ( centreDisplacement.first < 0 ) distanceToEdges.first *= -1.0;
-
-        }else if (closestEdges.first == closestEdges.second && closestEdges.first < 6) {
-            
-            //Calculate the displacements to edge
-            float delta = atan( fabs(edgeSize_ - centreDisplacement.first / centreDisplacement.second) );
-            float d = (edgeSize_*sqrt(3)/4) * 1.0/sin(5*PI/6 - delta);
-            distanceToEdges.second = 2.0 * (d - sqrt( pow(edgeSize_/2.0 - fabs(centreDisplacement.first), 2) + pow(centreDisplacement.second,2)))
-                                               * sin(5*PI/6 - delta);
-            distanceToEdges.first  = distanceToEdges.second/sqrt(3);
-
-            //Set the minus signs according to the associated edge
-            if (closestEdges.first == 0) {
-                distanceToEdges.second *= -1.0;
-            }else if (closestEdges.first == 2) {
-                distanceToEdges.first *= -1.0;
-                distanceToEdges.second *= -1.0;
-            }else if (closestEdges.first == 3) {
-                distanceToEdges.first *= -1.0;
-            }
-
+        float dY = fabs(fabs(centreDisplacement.second) - edgeSize_*sqrt(3)/2);
+        float b = fabs(edgeSize_*0.5 + (1/sqrt(3))*dY - fabs(centreDisplacement.first));
+        float c = dY;
+        if (fabs(centreDisplacement.first) > edgeSize_/2) {
+            c = b*sqrt(3);
         }
+
+        if (centreDisplacement.first < 0 && centreDisplacement.second > 0){
+            c *= -1.0;
+        }else if (centreDisplacement.first > 0 && centreDisplacement.second > 0){
+            b *= -1.0;
+            c *= -1.0;
+        }else if (centreDisplacement.first > 0 && centreDisplacement.second < 0){
+            b *= -1.0; 
+        }
+
+        distanceToEdges.first  = b;
+        distanceToEdges.second = c;
+
         return distanceToEdges;    
-
     }
-
-
     std::pair<float,float> Hexagon::getDisplacementFromCentre_XY(ROOT::Math::XYPoint hit) {
 
-        std::pair<float,float> displacementFromCentre(-999.,-999.);
+        std::pair<float,float> displacementFromCentre(9999.,9999.);
 
         displacementFromCentre.first  = hit.X() - centre_.X();
         displacementFromCentre.second = hit.Y() - centre_.Y();
 
         return displacementFromCentre;
-
     }
 
+    std::vector<float> Hexagon::getDistanceToEdges_UVW(ROOT::Math::XYPoint hit) {
 
+        UVWPoint centreUVW(centre_);
+        UVWPoint hitUVW(hit);
+        UVWPoint distFromCentre = hitUVW - centreUVW;
+
+        float dU = fabs(distFromCentre.getU() - edgeSize_*sqrt(3.0)/2.0);
+        if (distFromCentre.getU() > 0) dU *= -1.0;
+        float dV = fabs(distFromCentre.getV() - edgeSize_*sqrt(3.0)/2.0);
+        if (distFromCentre.getV() > 0) dV *= -1.0;
+        float dW = fabs(distFromCentre.getW() - edgeSize_*sqrt(3.0)/2.0);
+        if (distFromCentre.getW() > 0) dW *= -1.0;
+            
+        std::vector<float> edgeDists(3);
+        edgeDists[0] = dU;
+        edgeDists[1] = dV;
+        edgeDists[2] = dW;
+
+        return edgeDists;
+    }
 
 
 
